049b3cf142a9429904da219fc95aa63210edd06f
Emeric Henry <emeric.henry@gmail.com>
2023-04-27 18:40:16 +0000

    string format

--- authorization/test_authorization.py
+++ authorization/test_authorization.py
@@ -18,7 +18,7 @@ formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s",
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 if os.environ.get('LOG') == '1':
-    logger.info("Writing detailed log to {LOG_FILE}")
+    logger.info(f"Writing detailed log to {LOG_FILE}")
     file_handler = logging.FileHandler(LOG_FILE, encoding='utf-8-sig')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
